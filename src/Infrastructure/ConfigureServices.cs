3708bfd594120b6a5f6c112effff1dd0cdc38e54
Matheus Nani <matheus.nani@hotmail.com>
2022-09-13 16:51:36 -0600

    Add project files.

---
+++ src/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,56 @@
+﻿using CleanArchitectureSolution.Application.Common.Interfaces;
+using CleanArchitectureSolution.Infrastructure.Files;
+using CleanArchitectureSolution.Infrastructure.Identity;
+using CleanArchitectureSolution.Infrastructure.Persistence;
+using CleanArchitectureSolution.Infrastructure.Persistence.Interceptors;
+using CleanArchitectureSolution.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+
+        if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("CleanArchitectureSolutionDb"));
+        }
+        else
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        }
+
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+        services.AddScoped<ApplicationDbContextInitialiser>();
+
+        services
+            .AddDefaultIdentity<ApplicationUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddIdentityServer()
+            .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+        services.AddTransient<IDateTime, DateTimeService>();
+        services.AddTransient<IIdentityService, IdentityService>();
+        services.AddTransient<ICsvFileBuilder, CsvFileBuilder>();
+
+        services.AddAuthentication()
+            .AddIdentityServerJwt();
+
+        services.AddAuthorization(options =>
+            options.AddPolicy("CanPurge", policy => policy.RequireRole("Administrator")));
+
+        return services;
+    }
+}
---
+++ tests/Application.IntegrationTests/TodoLists/Commands/DeleteTodoListTests.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchitectureSolution.Application.Common.Exceptions;
+using CleanArchitectureSolution.Application.TodoLists.Commands.CreateTodoList;
+using CleanArchitectureSolution.Application.TodoLists.Commands.DeleteTodoList;
+using CleanArchitectureSolution.Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CleanArchitectureSolution.Application.IntegrationTests.TodoLists.Commands;
+
+using static Testing;
+
+public class DeleteTodoListTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireValidTodoListId()
+    {
+        var command = new DeleteTodoListCommand(99);
+        await FluentActions.Invoking(() => SendAsync(command)).Should().ThrowAsync<NotFoundException>();
+    }
+
+    [Test]
+    public async Task ShouldDeleteTodoList()
+    {
+        var listId = await SendAsync(new CreateTodoListCommand
+        {
+            Title = "New List"
+        });
+
+        await SendAsync(new DeleteTodoListCommand(listId));
+
+        var list = await FindAsync<TodoList>(listId);
+
+        list.Should().BeNull();
+    }
+}
